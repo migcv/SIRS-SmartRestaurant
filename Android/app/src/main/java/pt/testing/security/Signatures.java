3750dc0e8faa21fa658cc864630d4052d843f38b
marciosan <mrc.snts@gmail.com>
2016-12-03 21:51:11 +0000

    Signatures class has all signature related methods

---
+++ Android/app/src/main/java/pt/testing/security/Signatures.java
@@ -0,0 +1,184 @@
+package pt.testing.security;
+
+import java.io.*;
+
+import java.security.KeyStore;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Signature;
+import java.security.SignatureException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
+import java.util.Collection;
+
+import static javax.xml.bind.DatatypeConverter.printHexBinary;
+
+
+public class Signatures {
+
+	/**
+	 * Digest
+	 * @throws NoSuchAlgorithmException 
+	 * @throws IllegalArgumentException
+	 * 
+	 */
+	public static byte[] digest(String plainText) throws NoSuchAlgorithmException, IllegalArgumentException {
+		
+		if(plainText == null)
+			throw new IllegalArgumentException("Cannot digest a null string!");
+		
+		final byte[] plainBytes = plainText.getBytes();
+		
+		// DEBUG
+		System.out.println("Digest text:");
+		System.out.println(plainText);
+		System.out.println("Digest bytes:");
+		System.out.println(printHexBinary(plainBytes));
+		
+		// get a message digest object using the specified algorithm
+		MessageDigest messageDigest = MessageDigest.getInstance("SHA-1");
+		System.out.println(messageDigest.getProvider().getInfo());
+
+		System.out.println("Computing digest ...");
+		messageDigest.update(plainBytes);
+		byte[] digest = messageDigest.digest();
+
+		//DEBUG
+		System.out.println("Digest:");
+		System.out.println(printHexBinary(digest));
+		
+		return digest;
+	}
+
+
+	/**
+	 * Returns the public key from a certificate
+	 * 
+	 * @param certificate
+	 * @return
+	 */
+	public static PublicKey getPublicKeyFromCertificate(Certificate certificate) {
+		return certificate.getPublicKey();
+	}
+
+	/**
+	 * Reads a certificate from a file
+	 * 
+	 * @return
+	 * @throws Exception
+	 */
+	public static Certificate readCertificateFile(String certificateFilePath) throws Exception {
+		FileInputStream fis;
+
+		try {
+			fis = new FileInputStream(certificateFilePath);
+		} catch (FileNotFoundException e) {
+			System.err.println("Certificate file <" + certificateFilePath + "> not fount.");
+			return null;
+		}
+		BufferedInputStream bis = new BufferedInputStream(fis);
+
+		CertificateFactory cf = CertificateFactory.getInstance("X.509");
+
+		if (bis.available() > 0) {
+			Certificate cert = cf.generateCertificate(bis);
+			return cert;
+			// It is possible to print the content of the certificate file:
+			// System.out.println(cert.toString());
+		}
+		bis.close();
+		fis.close();
+		return null;
+	}
+
+	/**
+	 * Reads a collections of certificates from a file
+	 * 
+	 * @return
+	 * @throws Exception
+	 */
+	public static Collection<Certificate> readCertificateList(String certificateFilePath) throws Exception {
+		FileInputStream fis;
+
+		try {
+			fis = new FileInputStream(certificateFilePath);
+		} catch (FileNotFoundException e) {
+			System.err.println("Certificate file <" + certificateFilePath + "> not fount.");
+			return null;
+		}
+		CertificateFactory cf = CertificateFactory.getInstance("X.509");
+		@SuppressWarnings("unchecked")
+		Collection<Certificate> c = (Collection<Certificate>) cf.generateCertificates(fis);
+		fis.close();
+		return c;
+
+	}
+
+	/**
+	 * Reads a PrivateKey from a key-store
+	 * 
+	 * @return The PrivateKey
+	 * @throws Exception
+	 */
+	public static PrivateKey getPrivateKeyFromKeystore(String keyStoreFilePath, char[] keyStorePassword,
+			String keyAlias, char[] keyPassword) throws Exception {
+
+		KeyStore keystore = readKeystoreFile(keyStoreFilePath, keyStorePassword);
+		PrivateKey key = (PrivateKey) keystore.getKey(keyAlias, keyPassword);
+
+		return key;
+	}
+
+	/**
+	 * Reads a KeyStore from a file
+	 * 
+	 * @return The read KeyStore
+	 * @throws Exception
+	 */
+	public static KeyStore readKeystoreFile(String keyStoreFilePath, char[] keyStorePassword) throws Exception {
+		FileInputStream fis;
+		try {
+			fis = new FileInputStream(keyStoreFilePath);
+		} catch (FileNotFoundException e) {
+			System.err.println("Keystore file <" + keyStoreFilePath + "> not fount.");
+			return null;
+		}
+		KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
+		keystore.load(fis, keyStorePassword);
+		return keystore;
+	}
+
+	/** auxiliary method to calculate digest from text and cipher it */
+	public static byte[] makeDigitalSignature(byte[] bytes, PrivateKey privateKey) throws Exception {
+
+		// get a signature object using the SHA-1 and RSA combo
+		// and sign the plain-text with the private key
+		Signature sig = Signature.getInstance("SHA1WithRSA");
+		sig.initSign(privateKey);
+		sig.update(bytes);
+		byte[] signature = sig.sign();
+
+		return signature;
+	}
+
+	/**
+	 * auxiliary method to calculate new digest from text and compare it to the
+	 * to deciphered digest
+	 */
+	public static boolean verifyDigitalSignature(byte[] cipherDigest, byte[] bytes, PublicKey publicKey)
+			throws Exception {
+
+		// verify the signature with the public key
+		Signature sig = Signature.getInstance("SHA1WithRSA");
+		sig.initVerify(publicKey);
+		sig.update(bytes);
+		try {
+			return sig.verify(cipherDigest);
+		} catch (SignatureException se) {
+			System.err.println("Caught exception while verifying signature " + se);
+			return false;
+		}
+	}
+}
